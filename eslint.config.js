002a0879b2c6557167dcba41b1fabfa2b15375a9
Stephen Werronen <stephen.werronen@gmail.com>
2026-06-17 08:07:15 -0700

    feat: add eslint plugin to check jsx a11y

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
+  jsxA11y.flatConfigs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,4 +21,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
